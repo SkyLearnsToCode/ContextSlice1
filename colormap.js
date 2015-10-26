ad6ab34cc179f07d9742b47fb40586cf202a8a1b
TianyiLi <sky.litianyi@gmail.com>
2015-10-25 20:12:50 -0400

    Jaz: added color scheme, step text area and submit button

---
+++ colormap.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('person').css({"background-color":"orange"});
+  $('location').css({"background-color":"green"});
+  $('organization').css({"background-color":"pink"});
+  $('money').css({"background-color":"yellow"});
+  $('miscellanea').css({"background-color":"purple"});
+  $('phone').css({"background-color":"gray"});
+  $('interesting').css({"background-color":"red"});
+  $('date').css({"background-color":"blue"});
+});

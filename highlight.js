181a2dd93181d35e5808a09e870450892a6c26f4
TianyiLi <sky.litianyi@gmail.com>
2015-10-26 02:06:58 -0400

    add category without duplication, better layout

--- highlight.js
+++ highlight.js
@@ -11,12 +11,13 @@ function cancelCurrent(event){
 			newEntity += " ";
 		}
 	}
-	$(this).before(newEntity);
-	$(this).remove();
+	//$(this).before(newEntity);
+	$(this).replaceWith(newEntity);
+	parent[0].normalize();
 }
 
 function displayCategory (category){
-	alert(category);
+	$("#summary-cat").html("Category["+category+"]: ");
 	var entityList = "";
 	var cateEntities = $("em."+category);
 	for (i = 0; i<cateEntities.length; i++){
@@ -28,13 +29,14 @@ function displayCategory (category){
 
 function addNewCat(event){
 	//event.preventDefault();
-	var classN = $(this).text();
+	var classN = $(this).html();
 	if (window.getSelection) {  // all browsers, except IE before version 9
-		var entitySelect = document.getSelection();                                       
-		//alert (range.toString ());
-		if(entitySelect.rangeCount){
-			var entityStr = entitySelect.toString(); 
+		var entitySelect = document.getSelection(); 
+		var entityStr = entitySelect.toString();  
+		//alert(entitySelect.rangeCount);                                     
+		if(entitySelect.rangeCount && entityStr!=""){
 			var entityRange = entitySelect.getRangeAt(0);
+			//alert(entityRange);
 			var entityNode = document.createElement("em");
 			entityNode.className = "entity "+classN;
 			entityNode.appendChild(document.createTextNode(entityStr));
@@ -50,9 +52,37 @@ function addNewCat(event){
 	    }
 	}
 	displayCategory(classN);
+	if ($('input[name="editType"]:checked').val() == "cat"){
+		addNewCat(event);
+	}
+	//addNewCat(event);
 		//alert(entity.anchorNode.appendChild(entity.anchorNode));
 		//toString().anchor()
 }
 
+function addCategory(event){
+	$("#errorAlert").html("");
+	var cateName = $("input#newCat").val();
+	var existing = $("#catGroup button");
+	var add = true;
+	for (var n = 0; n < existing.length; n++){
+		if (cateName.toString().toLowerCase() === existing[n].innerHTML.toString().toLowerCase()){
+			var alertBanner = "<div class=\"alert alert-warning\"><strong>Error!</strong> This category already exists.</div>";
+			add = false;
+			$("#errorAlert").html(alertBanner);
+			break;
+		}
+	}
+	if(add && cateName!=""){
+		$("#catGroup").append($('<button/>', {
+	        text: cateName, 
+	        class: "btn btn-sm legend "+cateName
+    	}))
+	}
+}
+
+
 $("p").delegate(".entity","dblclick", cancelCurrent);
 $("div").delegate(".legend","click", addNewCat);
+$("#addCate").on("click",addCategory);
+
